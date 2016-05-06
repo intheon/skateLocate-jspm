e9ae3408e1efd39d7d7bf996b914de493c20eab0
Ben S <allobon@gmail.com>
2016-05-06 16:36:07 +0100

    woot

--- client/js/src/controllers/main.controller.js
+++ client/js/src/controllers/main.controller.js
@@ -1,20 +1,40 @@
 "use strict";
 
-function MainCtrl(allParksSrv, helpersSrv, stylesSrv, tagsSrv){
+function MainCtrl(allParksSrv, helpersSrv, stylesSrv, tagsSrv, NgMap){
+
+	let vm = this;
 
 	// Initialise array to store response from db
 	this.allData = [];
 	this.revData = [];
 	this.mapStyles = stylesSrv;
 	this.tags = tagsSrv;
+	vm.dynMarkers = []
 
 	allParksSrv.success((response) => {
 		this.allData = response;
 		this.revData = helpersSrv.rev(response);
 	}).then((response) => {
-		// do some more stuff
+
 
 	})
+
+}
+
+function groupMarkersIntoCluster(NgMap){
+
+	let dynMarkers = [];
+
+	NgMap.getMap().then((map) => {
+		let bounds = new google.maps.LatLngBounds();
+		angular.forEach(map.markers, (marker, key) => {
+			dynMarkers.push(marker);
+			bounds.extend(marker.getPosition());
+		});
+		let markerCluster = new MarkerClusterer(map, dynMarkers, {});
+		map.setCenter(bounds.getCenter());
+		map.fitBounds(bounds);
+	});
 
 }
 
